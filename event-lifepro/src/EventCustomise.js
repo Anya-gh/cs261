3e0bb3432126ac36c9f6c3f3ec6bc2b2e2685d27
akos979 <sz.akos979@gmail.com>
2021-02-19 16:09:50 +0000

    cosmetics on the page layouts

--- event-lifepro/src/EventCustomise.js
+++ event-lifepro/src/EventCustomise.js
@@ -6,29 +6,44 @@ function EventCustomise(){
     return (
         <div className="EventCustomise">
             <h1><u>Create event</u></h1>
-            <label for="Event Type">Choose a type of event: </label>
-            <select name="Event Type" id="Event Type">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <label for="Template">Choose a type of Template: </label>
-            <select name="Template" id="Template">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <input type="text" placeholder="Title"></input>
-            <input type="text" placeholder="Host Name"></input>
-            <input type="time"></input>
-            <label>to:</label>
-            <input type="time"></input>
-            <label for="AnalysisFreq">Choose analysis frequency: </label>
-            <select name="AnalysisFreq" id="AnalysisFreq">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
+            <p style={{textAlign: "center"}}>
+                <label for="Event Type">Choose a type of event: </label>
+                <select name="Event Type" id="Event Type">
+                    <option value="1">Session</option>
+                    <option value="2">Series of sessions</option>
+                    <option value="3">Project</option>
+                </select>
+            </p>
+            <p style={{textAlign: "center"}}>
+                <label>Enter title of the event: </label>
+                <input type="text" placeholder="Title"></input>
+                <br></br>
+                <label>Enter name of host: </label>
+                <input type="text" placeholder="Host Name"></input>
+            </p>
+            <p style={{textAlign: "center"}}>
+                <label for="Template">Choose a type of Template: </label>
+                <select name="Template" id="Template">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </p>
+            <p style={{textAlign: "center"}}>
+                <label>From </label>
+                <input type="time"></input>
+                <label> to: </label>
+                <input type="time"></input>
+            </p>
+            <p style={{textAlign: "center"}}>
+                <label for="AnalysisFreq">Choose analysis frequency: </label>
+                <select name="AnalysisFreq" id="AnalysisFreq">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+            </p>
+            <button>Create event</button>
         </div>
     );
 }
