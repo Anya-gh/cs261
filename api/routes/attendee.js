aa209af0937eff8c31d9ff9cc80845f6362e5be5
AgniArse <59710780+AgniArse@users.noreply.github.com>
2021-02-28 16:37:39 +0000

    Update attendee.js
    added function to check user.

--- api/routes/attendee.js
+++ api/routes/attendee.js
@@ -6,24 +6,8 @@ var prismadb = new PrismaClient();
 
 var app = express()
 app.use(express.json())
-/** async function keyexists(evID) {
-    //function to check if a exists
-    const keycheck = await prisma.event.findUnique({
-        where: {
-            eventID: evID,
-        }
-    })
-    if (keycheck) {
-        console.log(1);
-    } else {
-        console.log(2);
-    }
-} */
 
-
-/* GET users listing. */
-// default route, won't be using this in final product
-// use this for testing
+//GET eventID on checking a key, otherwise returns null
 router.get('/key/:id', async (req, res) => {
     const { id } = req.params;
     const keycheck = await prismadb.key.findUnique({
@@ -37,13 +21,32 @@ router.get('/key/:id', async (req, res) => {
     res.json(keycheck); //returns eventID to front end
 });
 //on front end side, it gets the response that the key code is valid
-//then must execute the create user by passing the attendee name and eventID
+//then must execute the check user by passing the attendee name
 
+// checks if a name provided exist, GET's userObject if so, otherwise null
+router.get('/check/:username', async (req, res) => {
+    const { username } = req.params;
+    var json = { name: username }
+    // ----- IMPORTANT-----
+    // may need the change the above var json based on what the userObject contains
+    // also the only query possible on json type is equal/not so format must be correct
+    const usercheck = await prismadb.user.findFirst({
+        where: {
+            userObject: {
+                equals: json,
+            }
+        },
+        select: {
+            userObject: true,
+        },
+    })
+    res.json(usercheck);
+})
+
+// creates a new user in the database given a name and the associated eventID
 router.post('/create', async(req, res) => {
     const { name, eventID } = req.body;
     const evint = parseInt(eventID);
-    console.log(evint);
-    console.log(eventID);
     //create user object function with name
     const result = await prismadb.user.create({
         data: {
@@ -51,7 +54,9 @@ router.post('/create', async(req, res) => {
             userObject: {  name: name }, // replace this with created user object
         }
     })
-    res.json(result) // result should be a separate const that hold userID associated event info
+    res.json(result) 
+    // result should be a separate const that hold userID associated event info
+    // null otherwise
 });
 
 module.exports = router;
