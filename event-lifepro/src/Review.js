a4dee58594fdd7168b1f37baa76812aef95210f2
Anya <aniket.singh@warwick.ac.uk>
2021-03-03 17:13:06 +0000

    Update Review.js

--- event-lifepro/src/Review.js
+++ event-lifepro/src/Review.js
@@ -4,18 +4,18 @@ import React from 'react';
 function Review({match}) {
 
     const [event, setEvent] = useState([]);
+    const [valid, setValid] = useState(false);
 
     useEffect(() => {
         getEvent();
     }, []);
 
     const getEvent = async () => {
-    // give api url (for database) and key to get event from db
-    // below is just a placeholder, (dont use this yet! it doesn't do anything, and it probably doesn't work).
-    const response = await fetch(`http://localhost:3000/${match.key}`);
-    const data = await response.json();
-    // take w/e attributes are needed from data
-    setEvent(data);
+        const response = await fetch(`http://localhost:3000/key/${match.key}`);
+        const data = await response.json();
+        console.log(data)
+        /*console.log(data.exist);
+        console.log(data.keycheck);*/
     };
 
     return(
